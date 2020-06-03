88f22462002b7b05609422f71d69f0a7fa39132a
Toby Boyne <tobyboyne@gmail.com>
2020-06-04 00:14:17 +0100

    Changed solve to store all possible paths to make each word

--- solver.py
+++ solver.py
@@ -10,6 +10,7 @@ from time import perf_counter
 
 from boggle import Boggle
 
+
 def get_next_moves(x, y, width, used_tiles):
 	"""Yields coordinates of possible next letters
 	Does not allow for coordinates outside of the board, or repeated use of the same tile"""
@@ -28,7 +29,7 @@ class Node:
 	def child_with_value(self, letter):
 		"""Returns the child with value `letter`."""
 		for child in self.children:
-			if child.v == letter.upper():
+			if child.v == letter:
 				return child
 		# if none exists:
 		return None
@@ -46,11 +47,9 @@ class Dictionary:
 	def __init__(self, src="CSW19"):
 
 		with open(os.path.join("dict", src+".json")) as infile:
-			self.lookup:dict = json.load(infile)
+			self.lookup = json.load(infile)
 		self.size = len(self.lookup)
-		t0 = perf_counter()
 		self.tree = self.make_tree()
-		print(perf_counter()-t0)
 
 	def __len__(self):
 		return self.size
@@ -75,26 +74,19 @@ class Dictionary:
 		return tree_head
 
 
-
-
-
-
 class Solver:
 	def __init__(self, dictionary, board):
 		self.d:Dictionary = dictionary
 		self.board = board
 
 	def solve(self):
-		print(self.board)
-
 		width = len(self.board)
 		starts = product(range(width), repeat=2)
 
 		tree_head = self.d.tree
 		# starts contains the coordinates for each tile in the grid
-		all_words = set()
+		all_words = {}
 		for x, y in starts:
-			print(all_words)
 			c = self.board[y][x]
 			prev_node = tree_head.child_with_value(c) # guaranteed to already exist
 			# each word is stored as [word_string, current_coords, [used_tiles], prev_node]
@@ -103,7 +95,6 @@ class Solver:
 
 			# keep looping until no new words can be made
 			while cur_words:
-				print(cur_words)
 				prev_words = cur_words
 				cur_words = []
 				for word in prev_words:
@@ -122,15 +113,23 @@ class Solver:
 							# check if this is a real word
 							# => has no children, or is in the dictionary
 							if not next_node.children or new_word[0] in self.d:
-								all_words.add(new_word[0])
+								new_string = new_word[0]
+								new_path = new_word[2]
+								# all_words stores the word, and all paths to make that word
+								all_words[new_string] = all_words.get(new_string, []) + [new_path]
 
 		print(all_words)
-
+		return all_words
 
 
 
 if __name__ == "__main__":
 	board = Boggle().gen()
+	t0 = perf_counter()
 	d = Dictionary()
+	print(f"Time to make tree: {perf_counter() - t0}")
+
 	s = Solver(d, board)
+	t1 = perf_counter()
 	s.solve()
+	print(f"Time to solve: {perf_counter() - t1}")
