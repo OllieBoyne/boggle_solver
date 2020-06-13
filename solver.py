982e214a2d0a296d206c3105b25ba179a73681e6
ollie <ollieboyne@gmail.com>
2020-06-13 12:54:17 +0100

    Further modifications to solver to improve computation speed

--- solver.py
+++ solver.py
@@ -9,19 +9,38 @@ from time import perf_counter
 
 from boggle import Boggle
 from dict import Dictionary
+import numpy as np
 
-from utils import Timer
+from utils import Timer, timeit
 timer = Timer()
 
 possible_moves = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1,-1], [1,0], [1,1]]
 
-def get_next_moves(x, y, width, used_tiles):
+clip = lambda X, width=5: np.clip(X, a_min=0, a_max = width-1)
+
+
+def gen_adjacents(width):
+	"""Given a board size <width>, generates a dict in which key (y, x) gives the list of valid (y, x) neighbour coordinates
+	not including itself
+
+	eg arr[0,0], width=5 = [(1,0), (0,1), (1,1)]
+	"""
+	adjacents = {} # np.zeros((width, width, width, width), dtype=np.bool)
+	for y in range(width):
+		for x in range(width):
+			y_start, y_end = sorted([0, y-1, y+2, width])[1:3]
+			x_start, x_end = sorted([0, x-1, x+2, width])[1:3]
+			# all valid coordinates, non-inclusive of x,y
+			adjacents[(y, x)] = [(j, i) for j in range(y_start, y_end) for i in range(x_start, x_end) if (i,j)!=(x,y)]
+
+	return adjacents
+
+def get_next_moves(adjacents, unused_tiles):
 	"""Yields coordinates of possible next letters
 	Does not allow for coordinates outside of the board, or repeated use of the same tile"""
-	for dx, dy in possible_moves:
-		X, Y = x + dx, y + dy
-		if 0 <= X < width and 0 <= Y < width and (X, Y) not in used_tiles:
-			yield X, Y
+
+	valid_moves = filter(lambda X: unused_tiles[X], adjacents)
+	return valid_moves
 
 def point_encoder(point, width=5):
 	"""Encodes coord as integer"""
@@ -34,46 +53,61 @@ def point_decoder(val, width=5):
 	return (x,y)
 
 class Solver:
-	def __init__(self, dict_src = "CSW19"):
+	def __init__(self, dict_src = "CSW19", width=5):
 		self.d = Dictionary(dict_src)
 
+		self.width = width
+
+		# numpy array of size (width)^4, which gives the adjacents mask for a given x,y. Generated initially to avoid repeats
+		self.adjacents = gen_adjacents(width)
+
 	def solve(self, board):
-		width = len(board)
+		assert len(board)==self.width, f"Board must be width {self.width} - got width {len(board)}"
+		width = self.width
 		starts = product(range(width), repeat=2)
 
 		tree_head = self.d.tree
 		# starts contains the coordinates for each tile in the grid
 		all_words = {}
 
+		# memory for storing unused tiles. Each word to be tried is given an index associated with this memory
+		unused_tiles = np.ones((1000, width, width), dtype=np.bool)
+
 		for x, y in starts:
 			c = board[y][x]
 			if c == "Q": c = "QU"
 			prev_node = tree_head[c] # guaranteed to already exist
 
-			# each word is stored as [word_string, current_coords, [used_tiles], prev_node]
-			first_word = [c, (x, y), [(x, y)], prev_node]
+			unused_tiles[:] = True # rest unused tiles memory
+			unused_tiles_counter = 0
+			unused_tiles[:, y, x] = 0
+
+			# each word is stored as [word_string, current_coords, idx_of_used_tiles, prev_node]
+			first_word = [c, (y, x), unused_tiles_counter, prev_node]
 			cur_words = [first_word]
 
 			# keep looping until no new words can be made
 			while cur_words:
 				prev_words = cur_words
 				cur_words = []
+
 				for word in prev_words:
-					word_string = word[0]
-					word_x, word_y = word[1]
-					used_tiles = word[2]
-					prev_node = word[3]
+					word_string, (word_y, word_x), unused_tiles_idx, prev_node = word
 
-					next_moves = get_next_moves(word_x, word_y, width, set(used_tiles))
+					next_moves = get_next_moves(self.adjacents[(word_y, word_x)], unused_tiles[unused_tiles_idx])
 
-					for i, j in next_moves:
+					for j, i in next_moves:
 						new_c = board[j][i]
 						if new_c == "Q":
 							new_c = "QU"
 						next_node = prev_node.get(new_c)
 
 						if next_node is not None:
-							new_word = [word_string + new_c, (i, j), used_tiles + [(i, j)], next_node]
+							unused_tiles_counter += 1
+							unused_tiles[unused_tiles_counter] = unused_tiles[unused_tiles_idx] # copy prev used tiles array
+							unused_tiles[unused_tiles_counter, j, i] = 0 # set this tile to used
+
+							new_word = [word_string + new_c, (j, i), unused_tiles_counter, next_node]
 							cur_words.append(new_word)
 							# check if this is a real word
 							# => has no children, or is in the dictionary
@@ -88,12 +122,26 @@ class Solver:
 if __name__ == "__main__":
 	board = Boggle()
 	s = Solver()
-	ts = []
-	N = 500
+
+	N = 300
 	t0 = perf_counter()
+	ts = []
+
 	for i in range(N):
 		s.solve(board.gen())
 		ts.append(perf_counter() - t0)
 		t0 = perf_counter()
+
 	print(f"Time to solve: {sum(ts)/len(ts)*1000:.1f}ms")
-	# print(timer.report(nits=N, make_dict=1))
+
+	# SOLVE BEST - EXPECTED ANSWER - 2945
+	t0 = perf_counter()
+	sol = s.solve(["EASAN", "BLRME", "AUIEE", "STSNS", "NURIG"])
+	print(sum(len(w)-3 for w in sol), f"[{(perf_counter()-t0) * 1000:.1f}ms]")
+	by_length = {}
+	for w in sol:
+		by_length[len(w)] = by_length.get(len(w), []) + [w]
+	print(", ".join([f"{i}: {len(v)}" for i, v in by_length.items()]))
+	print(by_length[11])
+
+	print(timer.report(nits=N, make_dict=1))
--- utils.py
+++ utils.py
@@ -34,19 +34,24 @@ class Timer:
 				else:
 					out_time = np.sum(t) / nits
 
-			out[k] = out_time
+			out[k] = {"out_time":out_time, "mean":np.mean(t), "call_count": len(t)}
 
-		return "\n".join([f"{k} = {t*1000:.1f}ms" for k, t in out.items()])
+		return "\n".join([f"{k} = {t['out_time']*1000:.1f}ms [{t['call_count']} calls, {t['mean']*1e6:.1f}us/call]" for k, t in out.items()])
 
-def timeit(func, *args, **kwargs):
+def timeit(func, N=1000000, *args, **kwargs):
 	"""Time the operation of a given function"""
 	t0 = perf_counter()
-	N = 10000
 	for n in range(N):
 		func(*args, **kwargs)
 	print(f"{func.__name__}, {(perf_counter()-t0)*1000000/N:.3f}us")
 
 if __name__ == "__main__":
 	time_funcs = [time, perf_counter, perf_counter_ns, process_time, monotonic, thread_time]
-	for f in time_funcs:
-		timeit(f)
+	# for f in time_funcs:
+	# 	timeit(f)
+
+	func1 = lambda y: sorted([0, y-1, y+2, 5])[1:3]
+	func2 = lambda y: (max(y-1,0), min(y+2,5))
+	func3 = lambda y: max(0, min(y, 5))
+
+	[timeit(f, y=3) for f in [func1, func2]]
